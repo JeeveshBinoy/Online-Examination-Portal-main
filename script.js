ce143719f9389dc5f615f060aabf0f1d69468fe1
Jeevesh Binoy <151763733+JeeveshBinoy@users.noreply.github.com>
2024-10-03 01:25:54 +0530

    commit

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 $(".logoutbtn").click(function() {
-    window.location.href = 'loginpage.html';
+    window.location.href = 'index.html';
 });
 
 $(".signinbtn").click(function(event) {
-     event.preventDefault();
-    window.location.href = 'studentdashboard.html';
+    event.preventDefault();
+    window.location.href = 'studentdashboard.html'; 
 });
 
-$(".examstartbtn").click(function(event){
+
+$(".startbtn").click(function(event){
     window.location.href = 'event.html';
 })
+
+$(".createexambtn").click(function(event){
+    window.location.href = 'createexam.html';
+})
+
+$(".studentdashboard").click(function(event){
+    window.location.href = 'studentdashboard.html';
+})
+
+$(".aboutbtn").click(function(event){
+    window.location.href = 'about.html';
+})
+
